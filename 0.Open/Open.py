79bc3000a064c2b33561d93e6a55a8a8634e4999
GSLAB-PC\GSLAB <U0552045@nkfust.edu.tw>
2020-03-29 19:26:36 +0800

    初次上傳
    1.讀取影像
    2.將影像灰階、二值化
    3.進行Open及Close個100次驗證效果

---
+++ 0.Open/Open.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import cv2
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
+img = cv2.imread('../WhyNot.jpg')
+img = cv2.imread('../WhyNot.jpg',0) #直接读为灰度图像
+
+cv2.imshow('org', img)
+ret,img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)#二值化
+cv2.imshow('threshold', img)
+
+for i in range(0,3):
+    for j in range(1,25):    
+        opening_25 = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, iterations=i*25+j)
+    cv2.imshow('open' +str(i), opening_25)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
