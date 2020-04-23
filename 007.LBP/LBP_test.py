05ff5c2d63ac2e449328530b91d700f636b616ab
HsuTengHsiung <55186419+HsuTengHsiung@users.noreply.github.com>
2020-04-23 13:11:58 +0800

    測試多尺度LBP(Multiscale Block LBP)

--- 007.LBP/LBP_test.py
+++ 007.LBP/LBP_test.py
@@ -9,13 +9,13 @@ from PIL import Image
 import cv2
 np.set_printoptions(threshold=np.inf)
 # settings for LBP
-radius = 5  # LBP算法中范围半径的取值
-n_points = radius*radius-1
+radius = 3  # LBP算法中范围半径的取值
+n_points = radius*8
 # =============================================================================
 # n_points = 8 * radius # 领域像素点数
 # =============================================================================
 # 读取图像
-image1 =cv2.imread('../098.picture/Road_5.jpg')
+image1 =cv2.imread('../098.picture/Road_6.jpg')
 cv2.imshow("image1",image1)
 
 plt.hist(image1.ravel(), 255, [0, 256])
@@ -37,8 +37,28 @@ plt.hist(gray.ravel(), 255, [0, 256])
 plt.show()
 
 
-lbp = local_binary_pattern(image1, n_points, radius)
-print(lbp.shape)
+lbp = local_binary_pattern(image1, n_points, radius,method ='ror')
+#**************************************************************************************
+Block_high = 5
+for i in range (0,int(1600/Block_high)):
+    for j in range(0,int(1200/Block_high)):
+        x=i*Block_high
+        y=j*Block_high
+        lbp2 = lbp[y:y+Block_high,x:x+Block_high]
+        print(lbp2.shape)
+        lbp2 = np.reshape(lbp2,(1,Block_high*Block_high))
+        lbp2 = lbp2[0]
+# =============================================================================
+#         print(lbp2)
+# =============================================================================
+        counts = np.bincount(list(map(float,lbp2)))
+        #返回众数
+        print("i=",i,"j=",j,np.argmax(counts))
+        lbp[y:y+Block_high,x:x+Block_high] = np.argmax(counts)
+        
+cv2.imwrite('../098.picture/Road_6_LBP_Block_test.jpg',lbp)
+
+#**************************************************************************************
 # =============================================================================
 # img_RGB = cv2.merge((lbp,lbp,))
 # cv2.imshow("img_RGB",img_RGB)
@@ -49,6 +69,8 @@ print(lbp.shape)
 # =============================================================================
 cv2.imshow("lbp",lbp)
 plt.hist(lbp.ravel(), 255, [0, 256])
+plt.show()
+plt.hist(lbp2.ravel(), 255, [0, 256])
 plt.show()
 # =============================================================================
 # print(lbp.ravel())
