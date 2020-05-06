c3676e821a5d7d4aaccad57fb4ac049ed695fa4a
GSLAB-PC\GSLAB <U0552045@nkfust.edu.tw>
2020-05-06 15:30:11 +0800

    Watershed
    嘗試自動找marker點，並自動上色

--- 007.LBP/Test2.py
+++ 007.LBP/Test2.py
@@ -1,42 +1,39 @@
-from skimage.transform import rotate
-from skimage.feature import local_binary_pattern
-from skimage import data, io,data_dir,filters, feature
-from skimage.color import label2rgb
-import skimage
-import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
 import cv2
+import numpy as np
 
-raw_image_gray=cv2.imread("../098.picture/BlockTest/LBP_noClose.jpg",0)
-raw_image=cv2.imread("../098.picture/Road/Road_5.jpg")
+
+raw_image_gray=cv2.imread("../098.picture/BlockTest/Road16.jpg",0)
+raw_image=cv2.imread("../098.picture/Road/Road_16.jpg")
 
 ret, markers = cv2.connectedComponents(raw_image_gray)
-cv2.imwrite('markers_0.jpg',markers*10)
-print(type(markers))
 plt.hist(markers.ravel(), 42, [-1, 40])
 plt.show()
+cv2.imwrite('markers_0.jpg',markers)
+print(type(markers))
 cv2.imshow("markers",markers )
 # =============================================================================
 # markers = markers*100 
-# =============================================================================
+# ===========================================================================
 # =============================================================================
 # cv2.imwrite('markers.jpg',markers)
 # =============================================================================
 
 
 markers = cv2.watershed(raw_image,markers)
-plt.hist(markers.ravel(), 42, [-1, 40])
-plt.show()
+# =============================================================================
+# plt.hist(markers.ravel(), 42, [-1, 40])
+# plt.show()
+# =============================================================================
+print( np.max(markers))
+print(len(markers))
 raw_image[markers == -1] = [255,255,51]
-raw_image[markers == 1] = [255,0,0]
-raw_image[markers == 2] = [0,255,0]
-raw_image[markers == 3] = [0,0,255]
-raw_image[markers == 5] = [255,255,255]
+for i in range(0,np.max(markers)):
+    raw_image[markers == i] =np.random.randint(0, 256, size=(1, 3))
 
 markers = markers*100 
-cv2.imwrite('markers.jpg',markers)
-cv2.imwrite('raw_image.jpg',raw_image )
+cv2.imwrite('../098.picture/markers.jpg',markers)
+cv2.imwrite('../098.picture/raw_image.jpg',raw_image )
 
 # cv2.imwrite('markers.jpg',markers)
 cv2.imshow("raw_image",raw_image)
